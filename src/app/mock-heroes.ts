312be7ef55a77d46325c20f6b524bf186c47b6b7
JuanBermu <201822159@tese.edu.mx>
2021-03-25 14:16:15 -0600

    Super heroe lista dinamica

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,25 @@
+import {Hero}from "./hero";
+
+export const HEROES: Hero[] = [
+
+{id: 11, name: "Superman", edad: 30, sp: "Super fuerza", identidad: "Clark Kent", universo: "DC"},
+
+{id: 12, name: "Batman", edad: 30, sp: "Artes marciales", identidad: "Bruce Wayne", universo: "DC"},
+
+{id: 13, name: "Hulk", edad: 40, sp: "Super fuerza", identidad: "Bruce Banner", universo: "Marvel"},
+
+{id: 14, name: "ironman", edad: 25, sp: "Fuerza sobrehumana", identidad: "Tony Stark", universo: "Marvel"},
+
+{id: 15, name: "Thor", edad: 1000, sp: "Rayo", identidad: "Thor Odinson", universo: "Marvel"},
+
+{id: 16, name: "Spiderman", edad: 20, sp: "Telaraña", identidad: "Peter Parker", universo: "Marvel"},
+
+{id: 17, name: "Black Widow", edad: 28, sp: "Agilidad", identidad: "Natasha Romanoff", universo: "Marvel"},
+
+{id: 18, name: "Flash", edad: 28, sp: "Super velocidad", identidad: "Barry Allen", universo: "DC"},
+
+{id: 19, name: "Bruja Escarlata", edad: 36, sp: "Magia", identidad: "Wanda Maximoff", universo: "Marvel"},
+
+{id: 20, name: "Wonder Woman", edad: 30, sp: "Super fuerza", identidad: "Diana de Temiscira", universo: "DC"}
+
+];
